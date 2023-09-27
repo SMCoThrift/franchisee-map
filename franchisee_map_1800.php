0e7ec4d81513779bbb55a8a33d30087e40d95c32
Michael Wender <michael@michaelwender.com>
2023-09-27 14:26:21 -0400

    Adding 1800GJ zip codes and franchisee map

---
+++ franchisee_map_1800.php
@@ -0,0 +1,8 @@
+<?php
+$franchisees_map = [
+  'Atlanta N.E. GJ' => 552350,
+  'Atlanta N.W. GJ' => 552345,
+  'Atlanta Southside GJ' => 552347,
+  'Kansas City GJ' => 552352,
+  'Phoenix GJ' => 552355,
+];
