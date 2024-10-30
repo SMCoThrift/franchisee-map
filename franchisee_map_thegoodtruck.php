a716db0eded011ada8d22ed95888a0c7e6dfee7b
Michael Wender <michael@michaelwender.com>
2024-10-30 12:24:15 -0400

    Adding Nov 2024 1800GJ, Adding The Good Truck

---
+++ franchisee_map_thegoodtruck.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * In the following, I have mapped all franchisees to
+ * the same Transportation Department: #589001 -
+ * The Good Truck.
+ *
+ * @var        array
+ */
+$franchisees_map = [
+  'Knoxville' => 589001,
+];
